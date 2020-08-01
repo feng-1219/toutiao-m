9c447f34c79e986700b87b4a9d33d1a74a254fe7
feng-1219 <1948919531@qq.com>
2020-08-01 23:47:04 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,20 @@
   <div id="app">
     <!-- 路由的出口 -->
     123
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <i class="toutiao toutiao-yuedu"></i>
+    </div>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
     <router-view />
   </div>
 </template>
 <script>
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
 <style lang="less"></style>
